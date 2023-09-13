d410e7cea572ad83bf9716713a3eda1fa694617c
Ibrokhim3 <ishamasiddinov.97@gmail.com>
2023-09-13 10:51:30 +0500

    test

---
+++ src/consts/apiUrl.js
@@ -0,0 +1,3 @@
+// export const API_URL = "https://invoices-ibrokhim.onrender.com/";
+// // "http://167.235.158.238:3001/";
+// // "http://localhost:1200/";
---
+++ src/store/config-store.js
@@ -0,0 +1,9 @@
+// import { configureStore } from "@reduxjs/toolkit";
+// import { invoicesReducer, userReducer } from "./invoices";
+
+// export const store = configureStore({
+//   reducer: {
+//     invoices: invoicesReducer,
+//     user: userReducer,
+//   },
+// });
