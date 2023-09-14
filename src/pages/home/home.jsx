e2948106430adbb9351423fd030d3e5d9ac9d606
Ibrokhim3 <ishamasiddinov.97@gmail.com>
2023-09-14 22:41:00 +0500

    bugs fixed

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -3,8 +3,13 @@ import { Header, MessageCard, SideBar } from "../../components"
 import "./home.scss"
 
 export const Home = ()=>{
-  return <ul className="jobs__list custom-flex">
+  return <div className="home">
+    <button className="home__create-btn custom-flex"><img width={44} height={44} src={alien} alt="create button" />Create a message</button>
+  <ul className="jobs__list custom-flex">
+  <MessageCard/>
+  <MessageCard/>
   <MessageCard/>
   <hr className="page__message-card-line" />
   </ul>
+  </div>
 }
