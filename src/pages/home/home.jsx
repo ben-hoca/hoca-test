47e7feff067ccb91ee7ff829e1ad757e605ac124
Ibrokhim3 <ishamasiddinov.97@gmail.com>
2023-09-15 10:26:27 +0500

    updated

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -1,4 +1,5 @@
-import { useState } from "react";
+import { Backdrop, CircularProgress } from "@material-ui/core";
+import { useEffect, useState } from "react";
 import alien from "../../assets/icons/alien.svg"
 import { Header, MessageCard, SideBar } from "../../components"
 import CreateMessage from "../../components/create-message/create-message";
@@ -11,8 +12,9 @@ export const Home = ()=>{
   const handleClose = () => {
     setOpenModal(false);
   };
-
+ 
   return <div className="home">
+
     <CreateMessage open={openModal} onClose={handleClose}/>
     <button onClick={()=>setOpenModal(true)} className="home__create-btn custom-flex"><img width={44} height={44} src={alien} alt="create button" />Create a message</button>
   <ul className="jobs__list custom-flex">
